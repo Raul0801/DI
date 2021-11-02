494e3ae3daa06a29339c2c392ffa4fd8e1e990dc
Raúl Carricoba Gómez <a20raulcg@ATEIS.LOCAL>
2021-11-02 19:12:04 +0100

    changed aliens project to adjust to activity requirements

---
+++ aliens/constants.py
@@ -0,0 +1,6 @@
+# game constants
+MAX_SHOTS = 3  # most player bullets onscreen
+ALIEN_ODDS = 18  # chances a new alien appears
+BOMB_ODDS = 50  # chances a new bomb will drop
+ALIEN_RELOAD = 12  # frames between new aliens
+SCORE = 0
